084080e3927a864f35bc0d436a66a5cedf452e42
Matthew Pagan <mastapegs01@gmail.com>
2021-06-12 10:41:50 -0400

    update flash card schema

--- schemas/flashCard.ts
+++ schemas/flashCard.ts
@@ -5,9 +5,33 @@ export const flashCard = {
   jsonType: "object",
   fields: [
     {
-      name: "question",
-      title: "Question",
-      type: "string",
+      name: "frontSide",
+      type: "object",
+      fields: [
+        {
+          name: "question",
+          title: "Question",
+          description: "This flash-card's question",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "backSide",
+      type: "object",
+      fields: [
+        {
+          name: "answer",
+          type: "string",
+          description: "This flash-card's answer",
+        },
+      ],
     },
   ],
+  preview: {
+    select: {
+      title: "frontSide.question",
+      subtitle: "backSide.answer",
+    },
+  },
 };
